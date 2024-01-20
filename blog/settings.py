1b635a438b4568328e8a8f7914fdfac428ad6ffe
devyoussouf <devyoussouf@MBP-de-APPLE.lan>
2024-01-20 11:21:30 +0100

    nexts_app

--- blog/settings.py
+++ blog/settings.py
@@ -139,6 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
@@ -156,8 +159,10 @@ STATICFILES_DIRS = [
 ]
 
 
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static'),
 MEDIA_URL ='/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
